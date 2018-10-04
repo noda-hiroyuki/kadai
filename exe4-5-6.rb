136a6cdf911d9a8c24269aed5be88d677be518c8
Noda Hiroyuki <axebomber13@gmail.com>
2018-10-04 14:02:45 +0900

    Ruby2_kadai

--- exe4-5-6.rb
+++ exe4-5-6.rb
@@ -1,8 +1,8 @@
-number=[1,2,3,4] #exe4
+number=[1,2,3,4]  #exe4
 
-num_array =number #exe5
+num_array =number  #exe5
 
-p num_array[0] #exe6
+p num_array[0]  #exe6
 p num_array[1]
 p num_array[2]
 p num_array[3]
